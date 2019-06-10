16f81ccea6591693f53b37003d1d61e93b114230
Thao Ha <thao.ha@soundcloud.com>
2019-06-10 19:03:38 +0200

    Support delete dag from cache file Fix update dag completion Remove related cache function from _airflow file

--- update_airflow_completion.zsh
+++ update_airflow_completion.zsh
@@ -16,16 +16,32 @@ update_full_airflow_completion(){
 }
 
 update_airflow_dag_completion() {
-        dag=${1}
-        echo "Finding task for $dag"
-        list_tasks=$(airflow list_tasks $dag; echo x)
-        task_lines=("${(f)list_tasks%x}")
-        tasks="${task_lines[@]:2}"
-        echo "\tFound: $tasks"
-        if [[ ( -f $COMPLETION_FILE) && $("grep -q "^$dag " $COMPLETION_FILE") ]]; then
-            $("sed -i '' -e "s/^$1.*/$dag $tasks/" $COMPLETION_FILE")
-        else
-            echo $dag $tasks >> $COMPLETION_FILE
-        fi
+        dag_name=${1}
+	list_dags=$(airflow list_dags | grep "$dag_name"; echo x)
+	lines=("${(f)list_dags%x}")
+	echo "Dags to update: $lines"
+	for dag in ${lines} 
+	do 
+		echo "Finding task for '$dag'"
+		list_tasks=$(airflow list_tasks $dag; echo x)
+		task_lines=("${(f)list_tasks%x}")
+		tasks="${task_lines[@]:2}"
+		echo "\tFound: $tasks"
+		# TODO: check if we found real tasks, or dags does not exist
+		if [[ -f $COMPLETION_FILE ]] && ( grep -q "^$dag " "$COMPLETION_FILE" ); then
+		    sed -i '' -e "s/^$dag .*/$dag $tasks/" $COMPLETION_FILE
+		else
+		    echo $dag $tasks >> $COMPLETION_FILE
+		fi
+	done
 }
 
+delete_dag_from_completion() {
+        dag=${1}
+	list_dags=$(airflow list_dags | grep "$dag"; echo x)
+	lines=("${(f)list_dags%x}")
+	echo "Dags to delete: $lines"
+	if [[ -f $COMPLETION_FILE ]] && ( grep -q "^$dag " "$COMPLETION_FILE" ); then
+		sed -i '' -e "/^$dag.*/d" "$COMPLETION_FILE"
+	fi
+}
